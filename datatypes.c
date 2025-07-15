bd8f66860defa001d2dc0dab3d89b93541ff7aed
P.V.S.L.Deepthi <161279349+pvsldeepthi-768@users.noreply.github.com>
2025-07-15 10:01:15 +0530

    Create datatypes.c

---
+++ datatypes.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    int roll;
+    char section = 'B';
+    float grade = 9.43;
+    int Class,sem;
+    roll=89;
+    Class=3;
+    sem=1;
+    printf('My infor-");
+    printf("Roll no:%d\n",roll);
+    printf("Class:%d  Semister:%d\n",Class,sem);
+    printf("section:%c\n",section);
+    printf("grade : %.2f\n",grade);
+    return 0;
+}
+/* output
+
+My infor-Roll no:89
+Class:3  Semister:1
+section:B
+grade : 9.43
+*/
