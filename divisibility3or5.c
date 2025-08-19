2eb9c4169c0530b3245476b0a4e9ea8b7182db0e
P.V.S.L.Deepthi <161279349+pvsldeepthi-768@users.noreply.github.com>
2025-08-19 10:17:13 +0530

    Create divisibility3or5.c

---
+++ divisibility3or5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter number:");
+    scanf("%d",&num);
+    if(num%3==0 && num%5==0){
+        printf("FizzBuzz");
+    }
+    else if(num%3==0){
+        printf("Fizz");
+    }
+    else if(num%5==0){
+        printf("Buzz");
+    }
+    else{
+        printf("Number is not divisible by 3 and 5");
+    }
+    return 0;
+}
+/*output
+Enter number:23
+Number is not divisible by 3 and 5
